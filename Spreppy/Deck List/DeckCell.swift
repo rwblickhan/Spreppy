b6c3b1418bbab52e1d37fc0e1591b3ad4d99732c
Russell Blickhan <rwblickhan@gmail.com>
2021-09-26 00:09:23 -0700

    Add table view cell for individual decks (#4)

---
+++ Spreppy/Deck List/DeckCell.swift
@@ -0,0 +1,42 @@
+//
+//  DeckCell.swift
+//  Spreppy
+//
+//  Created by Russell Blickhan on 9/25/21.
+//
+
+import Foundation
+import UIKit
+
+class DeckCell: UITableViewCell {
+    private lazy var label = makeLabel()
+    
+    init() {
+        super.init(style: .default, reuseIdentifier: nil)
+        
+        contentView.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalToSystemSpacingAfter: contentView.leadingAnchor, multiplier: 1.0),
+            label.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor),
+            label.topAnchor.constraint(equalToSystemSpacingBelow: contentView.topAnchor, multiplier: 1.0),
+            contentView.bottomAnchor.constraint(equalToSystemSpacingBelow: label.bottomAnchor, multiplier: 1.0)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) is unimplemented")
+    }
+    
+    func configure(with deckModel: DeckModel) {
+        label.text = deckModel.title
+    }
+    
+    // MARK: View Factories
+    
+    private func makeLabel() -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
+}
