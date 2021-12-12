86778620fe7b9d487a98e8864b2b9f48fd6693ce
Russell Blickhan <rwblickhan@gmail.com>
2021-12-11 16:20:11 -0800

    Create all the Leitner boxes

--- Spreppy/Features/Settings/SettingsViewModel.swift
+++ Spreppy/Features/Settings/SettingsViewModel.swift
@@ -43,7 +43,27 @@ class SettingsViewModel {
     func handle(_ event: SettingsUIEvent) {
         switch event {
         case .installBoxesTapped:
-            break
+            let apprentice1BoxUUID = UUID()
+            let apprentice2BoxUUID = UUID()
+            let apprentice3BoxUUID = UUID()
+            let apprentice4BoxUUID = UUID()
+            let guru1BoxUUID = UUID()
+            let guru2BoxUUID = UUID()
+            let masterBoxUUID = UUID()
+            let enlightenedBoxUUID = UUID()
+            let apprentice1Box = LeitnerBoxModel(
+                uuid: apprentice1BoxUUID,
+                title: String(localized: "Apprentice 1"),
+                delayBeforeDisplay: 0,
+                previousStageUUID: nil,
+            nextStageUUID: apprentice2BoxUUID)
+            let apprentice2Box = LeitnerBoxModel(uuid: apprentice2BoxUUID, title: String(localized: "Apprentice 2"), delayBeforeDisplay: 4 * 60 * 60, previousStageUUID: apprentice1BoxUUID, nextStageUUID: apprentice3BoxUUID)
+            let apprentice3Box = LeitnerBoxModel(uuid: apprentice3BoxUUID, title: String(localized: "Apprentice 3"), delayBeforeDisplay: 8 * 60 * 60, previousStageUUID: apprentice2BoxUUID, nextStageUUID: apprentice4BoxUUID)
+            let apprentice4Box = LeitnerBoxModel(uuid: apprentice4BoxUUID, title: String(localized: "Apprentice 4"), delayBeforeDisplay: 24 * 60 * 60, previousStageUUID: apprentice3BoxUUID, nextStageUUID: guru1BoxUUID)
+            let guru1Box = LeitnerBoxModel(uuid: guru1BoxUUID, title: String(localized: "Guru 1"), delayBeforeDisplay: 2 * 24 * 60 * 60, previousStageUUID: apprentice4BoxUUID, nextStageUUID: guru2BoxUUID)
+            let guru2Box = LeitnerBoxModel(uuid: guru2BoxUUID, title: String(localized: "Guru 2"), delayBeforeDisplay: 7 * 24 * 60 * 60, previousStageUUID: guru1BoxUUID, nextStageUUID: masterBoxUUID)
+            let masterBox = LeitnerBoxModel(uuid: masterBoxUUID, title: String(localized: "Master"), delayBeforeDisplay: 2 * 7 * 24 * 60 * 60, previousStageUUID: guru2BoxUUID, nextStageUUID: enlightenedBoxUUID)
+            let enlightenedBox = LeitnerBoxModel(uuid: enlightenedBoxUUID, title: String(localized: "Enlightened"), delayBeforeDisplay: 4 * 7 * 24 * 60 * 60, previousStageUUID: masterBoxUUID, nextStageUUID: nil)
         }
     }
 }
